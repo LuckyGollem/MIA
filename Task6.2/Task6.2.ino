9ddd823860e8d681f56128f5b4c2fd308d2d6b77
LuckyGollem <123700616+LuckyGollem@users.noreply.github.com>
2023-09-23 16:31:28 +0300

    Task6.2.ino

--- Task6.2/Task6.2.ino
+++ Task6.2/Task6.2.ino
@@ -3,33 +3,29 @@
 
 int Counts = 0;
 
-int LastAState;
 int UpdateAState;
 int UpdateBState;
+
+void InterruptFunc(){
+    if(digitalRead(OutputA) != digitalRead(OutputB)){
+      Counts++;
+    } else{
+      Counts--;
+    }
+}
 
 void setup() {
   //Initializing pins
   pinMode(OutputA, INPUT);
   pinMode(OutputB, INPUT);
-  //Initializing the first state of pin A
-  LastAState = digitalRead(OutputA);
-  
+
+  attachInterrupt(digitalPinToInterrupt(OutputA), InterruptFunc, CHANGE);
+
   Serial.begin(9600);
 }
 
 void loop() {
-  //Getting the new pin A and pin B states
-  UpdateAState = digitalRead(OutputA);
-  UpdateBState = digitalRead(OutputB);
+
   //Seeing if pin A changed states
-  if(UpdateAState != LastAState){ 
-    if(UpdateAState != UpdateBState){
-      Counts++;
-    } else{
-      Counts--;
-    }
-  }
   Serial.println(Counts/2);
-  //Getting the state of pin A for the next iteration
-  LastAState = UpdateAState;
 }
